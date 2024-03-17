e0e924422ed4049c432167bf3bc6f75c308dc9fc
nguyennt02 <nguyennt19apr@gmail.com>
2024-03-17 15:38:01 +0700

    update

--- Scripts/Life/LeftActive.cs
+++ Scripts/Life/LeftActive.cs
@@ -5,6 +5,7 @@ public class LeftActive : MonoBehaviour, IActive
     public void Active(GameObject player)
     {
         player.GetComponent<IHealing>().Healing(1);
+        AudioMainManager.Instance.PlaySFX("Furit");
         Destroy(gameObject);
     }
 }
